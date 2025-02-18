6c20682777d66e6c5acb3816e77b9562723da00a
Izendi <cjwinters1404@gmail.com>
2025-02-18 19:33:58 +0100

    prep for RC sampling

--- shaders/fs_RCFinal.glsl
+++ shaders/fs_RCFinal.glsl
@@ -12,7 +12,14 @@ uniform sampler2D u_tex_3;
 
 void main()
 {
+
+    //#HERE You need to sample the corresponding RC data depending on the fragments position and average it with all 4 directions, 
+    // each of the 4 directions should be merged and average with the above levels, just like was specified in the paper, you should be able to do
+    // All of that here in this shader, assuming you have passed in the corresponding RC texture data and know the rules for where sampling should occur.
+
+
     vec4 texColor = texture(u_tex_3, texCoord);
     //FragColor = vec4(0.7, 0.2, 0.5, 1.0);
     FragColor = vec4(texColor.x, texColor.y, texColor.z, 1.0);
+
 }
